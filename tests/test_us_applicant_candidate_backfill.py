0094489e88c291b1b64e2c329875c334463c45c4
whalemarks <jasondon@gmail.com>
2026-09-15 10:15:59 +0800

    fix: raise bounded applicant reconcile read budget

--- tests/test_us_applicant_candidate_backfill.py
+++ tests/test_us_applicant_candidate_backfill.py
@@ -128,6 +128,7 @@ def test_reconcile_inserts_only_bounded_missing_bindings():
     assert "target.record_hash = source.record_hash" in client.queries[0][0]
     assert "LIMIT 3" in client.queries[0][0]
     assert client.inserts[0][0] == APPLICANT_INDEX_TABLE
+    assert client.queries[0][1]["max_rows_to_read"] == 150_000_000
 
 
 def test_reconcile_fails_closed_above_bound():
